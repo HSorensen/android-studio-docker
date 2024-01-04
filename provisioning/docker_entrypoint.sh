34a06415d3d4822ccc3eaaf5046439ba9814ce80
hsorensen <hsorensen@rodanandfields.com>
2024-01-04 08:10:15 -0800

    docker_emtrypoint.sh: various fixes
    - chown android:android for /studio-data/profile/android
    - chown android:android for /studio-data/profile/java
    - touch analytics.settings
    - mkdir .java/.systemPrefs
    - chown android:android for .java/.systemPrefs

--- provisioning/docker_entrypoint.sh
+++ provisioning/docker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Change permissions of /dev/kvm for Android Emulator
-echo "`whoami`" | sudo -S chmod 777 /dev/kvm > /dev/null 2>&1
+#echo "`whoami`" | sudo -S chmod 777 /dev/kvm > /dev/null 2>&1
 
 export PATH=$PATH:/studio-data/platform-tools/
 
@@ -10,6 +10,15 @@ if [ ! -d "/studio-data/Android" ]; then
   mkdir -p /studio-data/Android
 fi
 sudo chown -R android:android /studio-data/Android
+
+sudo chown -R android:android /studio-data/profile/android
+
+touch /studio-data/profile/android/analytics.settings
+
+sudo chown -R android:android /studio-data/profile/java
+
+mkdir -p .java/.systemPrefs
+sudo chown -R android:android .java/.systemPrefs
 
 # Default to 'bash' if no arguments are provided
 args="$@"
